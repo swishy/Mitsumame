73987d6b93e88bd41ce3738f55c98cb08b2d9752
Dale Anderson <danderson@joobworld.com>
2013-10-25 13:01:13 +1300

    Add initial Postgres storage of users, update gradle.build to add postgres driver, add hibernate.properties config.

--- src/main/kotlin/storage/provider/user.kt
+++ src/main/kotlin/storage/provider/user.kt
@@ -4,6 +4,9 @@ import java.util.HashMap
 import storage.UserStorageProvider
 import storage.types.User
 import java.util.UUID
+import org.hibernate.SessionFactory
+import org.hibernate.cfg.Configuration
+import org.slf4j.LoggerFactory
 
 /**
  * Created with IntelliJ IDEA.
@@ -26,6 +29,38 @@ public class InMemoryUserStorageProvider : UserStorageProvider
 
     override fun getUser(userId: UUID): User? {
         return currentUsers.get(userId)
+    }
+
+}
+
+public class HibernateUserStorageProvider : UserStorageProvider
+{
+    private var log = LoggerFactory.getLogger(javaClass<HibernateUserStorageProvider>())
+
+    val factory = Configuration().configure()?.buildSessionFactory();
+
+    override fun getUser(userId: UUID): User? {
+        var session = factory?.openSession()
+        var user = session?.get(javaClass<User>(), userId) as User;
+        return user;
+    }
+
+    override fun deleteUser(userId: UUID) {
+        var user = getUser(userId)
+        var session = factory?.openSession()
+        session?.delete(user)
+    }
+
+    override fun storeUser(user: User) {
+        var session = factory?.openSession()
+        try
+        {
+            session?.save(user)
+        }
+        catch(exception : Exception)
+                {
+                    log?.debug("Error saving user ${user.userName}")
+                }
     }
 
 }
