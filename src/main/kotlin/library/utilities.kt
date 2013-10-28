76878b7d2bf9ea7e53a0e83920bd819fe3594504
Stefan Park <obsoleteacey@gmail.com>
2013-10-28 22:36:05 +1300

    Add encryption utils.  NO decrypt yet!

--- src/main/kotlin/library/utilities.kt
+++ src/main/kotlin/library/utilities.kt
@@ -7,6 +7,15 @@ package com.st8vrt.mitsumame.utilities
 import java.util.HashMap
 import java.net.URI
 import org.slf4j.LoggerFactory
+import org.apache.commons.codec.binary.Base64
+import javax.crypto.KeyGenerator
+import java.security.SecureRandom
+import javax.crypto.SecretKey
+import javax.crypto.SecretKeyFactory
+import javax.crypto.spec.PBEKeySpec
+import javax.crypto.spec.SecretKeySpec
+import javax.crypto.Cipher
+import javax.crypto.spec.IvParameterSpec
 
 public var rootDocumentEntries : HashMap<String, URI> = HashMap<String, URI>()
 
@@ -37,4 +46,65 @@ public class SessionUtilities
         log!!.info("Session Id: [${sessionId}]")
         return true;
     }
+}
+
+public class EncryptionUtilities
+{
+    private var salt = "Adfas" + 24576 + "6&#N%^BW" + ",.|%^&*"
+
+    private var log = LoggerFactory.getLogger(javaClass<SessionUtilities>())
+
+    public fun encryptString(message : String, key : ByteArray) : String  {
+        var iv = getRandomIv();
+
+        return Base64.encodeBase64String(iv) + encryptString(message, key, salt.getBytes(), iv as ByteArray);
+    }
+
+    public fun encryptString(plainText:String, encryptionKey:ByteArray, salt:ByteArray, iv:ByteArray) : String {
+
+        var ivToUse = iv ?: getRandomIv()
+
+        try{
+            var factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1")
+
+            var charEncryptionKey = encryptionKey.toCharArray();
+
+            var keyspec = PBEKeySpec(charEncryptionKey, salt, 1024, 16);
+            var secretKey = factory?.generateSecret(keyspec);
+
+            var cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+            cipher?.init(Cipher.ENCRYPT_MODE, SecretKeySpec(secretKey?.getEncoded(), "AES")
+                    , IvParameterSpec(iv));
+
+            return Base64.encodeBase64String(cipher?.doFinal(plainText.getBytes())) as String
+        } catch(e:Exception) {
+            log?.error("Unable to encrypt message", e)
+            throw Exception("Unable to encrypt message")
+        }
+    }
+
+    public fun getRandomIv() : ByteArray? {
+        try {
+            var keyGen = KeyGenerator.getInstance("AES")
+            var sr = SecureRandom.getInstance("SHA1PRNG")
+            keyGen?.init(128, sr)
+            var secretKey = keyGen?.generateKey()
+            return secretKey?.getEncoded()
+        } catch(e : Exception) {
+            log?.error("Unable to create random IV", e)
+            throw Exception("Unable to create random IV")
+        }
+    }
+}
+
+public fun ByteArray.toCharArray() : CharArray? {
+    var charArray = CharArray(this.size)
+
+    var count = 0
+    for(byteVal in this) {
+        charArray[0] = byteVal.toChar();
+        count++
+    }
+
+    return charArray
 }
